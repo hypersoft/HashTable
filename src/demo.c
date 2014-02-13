bd394c223baa10915a3eaa24feab5740cfe9b6d9
Triston J. Taylor <pc.wiz.tt@gmail.com>
2014-02-13 11:23:39 -0600

    Lazy demo
    Demonstration of typical HashTable usage.

--- src/demo.c
+++ src/demo.c
@@ -11,12 +11,38 @@ int main ( int argc, char **argv )
 	char * key = "double.value";
 	double value = 123.444;
 
-	if ( HashTablePutDouble(hashTable, key, value, true) ) {
-		double got = HashTableGetDouble(hashTable, key);
-		printf ( "%s == %g\n", key, got );
-	} else {
+	if ( ! HashTablePutDouble(hashTable, key, value, true) ) {
 		e = HashTableGetLastError ( hashTable );
 		fprintf(stderr, "%s: Couldn't put %s: Last Error: %lu\n",
+			argv[0], key, e
+		);
+		return e;
+	}
+
+	if (HashTableHasEntry(hashTable, key)) {
+		double got = HashTableGetDouble(hashTable, key);
+		e = HashTableGetLastError(hashTable);
+		if (!e) printf ( "%s == %g\n", key, got );
+		else {
+			fprintf(stderr, "%s: Couldn't get %s: Last Error: %lu\n",
+				argv[0], key, e
+			);
+			return e;
+		}
+	}
+
+	if (HashTableDelete(hashTable, "not-a-key")) {
+			fprintf(stderr, "%s: Deleted a non-existant key!\n",
+				argv[0]
+			);
+			return 1;
+	} else HashTableGetLastError(hashTable); // belay that order...
+
+	if (HashTableDelete(hashTable, key)) {
+			printf("%s: Succesfully deleted %s\n", argv[0], key);
+	} else {
+		e = HashTableGetLastError(hashTable);
+		fprintf(stderr, "%s: Couldn't delete %s: Last Error: %lu\n",
 			argv[0], key, e
 		);
 		return e;
